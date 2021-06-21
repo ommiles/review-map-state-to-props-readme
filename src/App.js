8fc05a7970953819981c9ea7a078263ba2f8f60c
OM Miles <miles@ommiles.com>
2021-06-21 08:15:12 -0700

    readme completed

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ class App extends Component {
   handleOnClickUsers() {
     this.props.dispatch({
       type: 'GET_COUNT_OF_USERS',
-    });
+    })
   }
 
   render() {
-    // debugger;
+    debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
@@ -27,15 +27,19 @@ class App extends Component {
           <button onClick={() => this.handleOnClickUsers()}>
             Click to change user count
           </button>
-          <p>{this.props.items.length}</p>
+          <p> {this.props.items.length}</p>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  debugger;
+  // debugger;
   return { items: state.items }
 }
 
+// the connect() function is used for us to connect our Redux part 
+// of the application to the React part of the application...
 export default connect(mapStateToProps)(App);
+// Refactor below:
+// export default connect( state => ({ items: state.items }) )(App); 
